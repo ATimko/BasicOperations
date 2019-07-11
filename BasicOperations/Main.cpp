003b21276d20931e147843a8b87a7cdb3b88f54f
ATimko <ashleytimko777@gmail.com>
2019-07-11 14:39:59 -0700

    Add project files.

---
+++ BasicOperations/Main.cpp
@@ -0,0 +1,22 @@
+//#define _USE_TIME_DEFINES
+
+using namespace std;
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+const long SECONDS_PER_MINUTE = 60L;
+const long MINUTES_PER_HOUR = 60L;
+const long HOURS_PER_DAY = 24L;
+
+int main(){
+	//using std::cout;
+	//using std::endl;
+
+	cout << "Enter a number of seconds: ";
+
+
+	system("pause");
+	return 0;
+}
